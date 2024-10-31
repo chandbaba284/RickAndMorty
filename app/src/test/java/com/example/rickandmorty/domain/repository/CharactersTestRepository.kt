b2d7671dbe1f06f78a5abbf367680f45bb410f42
cshaik <cshaik@griddynamics.com>
2024-10-31 16:00:08 +0530

    modified test cases as per new character schema

--- app/src/test/java/com/example/rickandmorty/domain/repository/CharactersTestRepository.kt
+++ app/src/test/java/com/example/rickandmorty/domain/repository/CharactersTestRepository.kt
@@ -3,6 +3,7 @@ package com.example.rickandmorty.domain.repository
 import androidx.paging.PagingData
 import com.exmple.rickandmorty.GetCharactersQuery
 import com.exmple.rickandmorty.fragment.Character
+import com.exmple.rickandmorty.fragment.Location
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import repository.CharactersRepository
@@ -16,8 +17,8 @@ class CharactersTestRepository : CharactersRepository {
                     Character(
                         name = "",
                         "",
-                        "",
-                        Character.Location("", ""),
+                        "","","","",
+                        location = Character.Location("", Location("","",""))
                     ),
                 ),
                 GetCharactersQuery.Result(
@@ -25,11 +26,11 @@ class CharactersTestRepository : CharactersRepository {
                     Character(
                         name = "",
                         "",
-                        "",
-                        Character.Location("", ""),
+                        "","","","",
+                        Character.Location("", Location("","",""),
                     ),
                 ),
-            )
+            ))
 
         return flow { PagingData.from(items) }
     }
