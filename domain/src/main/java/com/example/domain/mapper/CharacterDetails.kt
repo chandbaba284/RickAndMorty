8746a8ec27fdc984c36a6b765df6a66515e38fb7
cshaik <cshaik@griddynamics.com>
2024-10-31 15:54:07 +0530

    implemented data domain layers for character details screen

---
+++ domain/src/main/java/com/example/domain/mapper/CharacterDetails.kt
@@ -0,0 +1,10 @@
+package com.example.domain.mapper
+
+data class CharacterDetails(
+    val id: String? = "",
+    val name: String? = "",
+    val image: String? = "",
+    val status: String? = "",
+    val species: String? = "",
+    val gender: String? = ""
+)
---
+++ domain/src/main/java/com/example/domain/repository/CharacterDetailsRepository.kt
@@ -0,0 +1,7 @@
+package com.example.domain.repository
+
+import com.example.domain.mapper.CharacterDetails
+
+interface CharacterDetailsRepository {
+    suspend fun getCharacterDetailsById(id : String) : CharacterDetails?
+}
