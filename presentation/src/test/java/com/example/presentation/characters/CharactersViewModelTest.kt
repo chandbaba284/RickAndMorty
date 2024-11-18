1966a4df387e67d5638df58fe0688cf79fc47ffe
cshaik <cshaik@griddynamics.com>
2024-11-18 15:04:17 +0530

    removed differ from View Model test

--- presentation/src/test/java/com/example/presentation/characters/CharactersViewModelTest.kt
+++ presentation/src/test/java/com/example/presentation/characters/CharactersViewModelTest.kt
@@ -1,10 +1,6 @@
 package com.example.presentation.characters
 
-import android.util.Log
-import androidx.paging.AsyncPagingDataDiffer
 import androidx.paging.PagingData
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListUpdateCallback
 import app.cash.turbine.test
 import com.example.presentation.uistate.UiState
 import com.example.presentation.viewmodel.CharactersViewModel
@@ -14,12 +10,11 @@ import com.exmple.rickandmorty.fragment.Location
 import com.google.common.truth.Truth.assertThat
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.every
 import io.mockk.mockk
-import io.mockk.mockkStatic
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
@@ -38,8 +33,6 @@ class CharactersViewModelTest {
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-        mockkStatic(Log::class)
-        every { Log.isLoggable(any(), any()) } returns false
         Dispatchers.setMain(testDispatcher)
         charactersViewModel = CharactersViewModel(useCase)
 
@@ -47,38 +40,56 @@ class CharactersViewModelTest {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun givenValidCharacters_whenInvokeIsCalledInUseCase_thenEmitsMockedPagingDataToStateFlow() =
+    fun givenValidCharacters_whenInvokeIsCalledInUseCase_thenListSizeShouldMatchWithStateFlowItemsSize() =
         runTest(testDispatcher) {
-            val mockPagingData = getCharacters()
-            coEvery { useCase.invoke() } returns flowOf(mockPagingData)
+            //Given
+            val items =
+                listOf(
+                    GetCharactersQuery.Result(
+                        "",
+                        Character(
+                            name = "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            location = Character.Location("", Location("", "", "")),
+                            Character.Origin("",""), episode = listOf(Character.Episode("","",""))
+                        ),
+                    ),
+                    GetCharactersQuery.Result(
+                        "",
+                        Character(
+                            name = "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            Character.Location(
+                                "",
+                                Location("", "", ""),
+                            ),
+                            Character.Origin("",""), episode = listOf(Character.Episode("","",""))
+                        ),
+
+                        ),
+                )
+
+            val pagedData = PagingData.from(items)
+            //When
+            coEvery { useCase.invoke() } returns flowOf(pagedData)
+            //Then
             charactersViewModel.charactersState.test {
                 charactersViewModel.fetchData()
                 assertThat(awaitItem()).isEqualTo(UiState.Loading)
                 advanceUntilIdle()
                 val successState = awaitItem()
                 assertThat(successState).isInstanceOf(UiState.Success::class.java)
-                val differ = AsyncPagingDataDiffer(
-                    diffCallback = object : DiffUtil.ItemCallback<GetCharactersQuery.Result>() {
-                        override fun areItemsTheSame(
-                            oldItem: GetCharactersQuery.Result,
-                            newItem: GetCharactersQuery.Result
-                        ) = oldItem.character.id == newItem.character.id
-
-                        override fun areContentsTheSame(
-                            oldItem: GetCharactersQuery.Result,
-                            newItem: GetCharactersQuery.Result
-                        ) = oldItem == newItem
-                    },
-                    updateCallback = NoopListUpdateCallback(),
-                    mainDispatcher = testDispatcher,
-                    workerDispatcher = testDispatcher
-                )
-                differ.submitData((successState as UiState.Success).data)
-                val actualCharacters = differ.snapshot().items
-                differ.submitData(mockPagingData)
-                val expectedCharacters = differ.snapshot().items
-                assertThat(actualCharacters).isEqualTo(expectedCharacters)
-
+                val expectedCharacters = useCase.invoke().toList()
+                val actualCharacters = flowOf((successState as UiState.Success).data).toList()
+                assertThat(actualCharacters.size).isEqualTo(expectedCharacters.size)
                 cancelAndConsumeRemainingEvents()
 
             }
@@ -90,48 +101,5 @@ class CharactersViewModelTest {
         Dispatchers.resetMain()
     }
 
-    private fun getCharacters(): PagingData<GetCharactersQuery.Result> {
-        val items =
-            listOf(
-                GetCharactersQuery.Result(
-                    "",
-                    Character(
-                        name = "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        location = Character.Location("", Location("", "", "")),
-                        Character.Origin("",""), episode = listOf(Character.Episode("","",""))
-                    ),
-                ),
-                GetCharactersQuery.Result(
-                    "",
-                    Character(
-                        name = "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        Character.Location(
-                            "",
-                            Location("", "", ""),
-                        ),
-                        Character.Origin("",""), episode = listOf(Character.Episode("","",""))
-                    ),
 
-                ),
-            )
-
-        return PagingData.from(items)
-    }
-
-}
-class NoopListUpdateCallback : ListUpdateCallback {
-    override fun onInserted(position: Int, count: Int) {}
-    override fun onRemoved(position: Int, count: Int) {}
-    override fun onMoved(fromPosition: Int, toPosition: Int) {}
-    override fun onChanged(position: Int, count: Int, payload: Any?) {}
 }
